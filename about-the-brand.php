5f1309cea1e734c89f67fbca39936feca67e319e
aleem93 <aleemjaved93@gmail.com>
2017-07-24 18:32:11 +0530

    voyy about page design

--- about-the-brand.php
+++ about-the-brand.php
@@ -11,12 +11,24 @@
   </head>
   <body>
     <?php include 'includes/header.php'; ?>
-     <section>
-       <img src="images/about-us-banner.jpg" class="img-responsive" alt="about-us-banner" width=" " height=" " />
-     </section>
-    <section>
-      <img src="images/under-construction.jpg" class="img-responsive" alt="under-construction" width=" " height=" " />
-    </section> 
+    <section class="top_banner">
+      <img src="images/category-banner-bg2.jpg" class="img-responsive" alt="category-banner-bg" width=" " height=" " />
+      <div class="caption">
+        <h1 class="tittle">About The Brand</h1>
+        <p>Something different, every day.</p>
+      </div>
+    </section>
+    <section class="about">
+      <div class="container">
+        <div class="blocks">
+        <div class="block_img"><img src="images/about-sec-img.jpg" class="img-responsive" alt=""></div>
+        <div class="block_info">
+          <h2 class="heading">About The Brand</h2>
+          <p>Lorem ipsum dolor sit amet, ea eum ignota inimicus, in atqui persius partiendo sit, no sit iusto quaestio. Ad pri convenire incorrupte, possim forensibus eum et. Postea diceret interesset ea nam. In amet augue falli ius, illum placerat sed ei, duo cu impetus laoreet persecuti. Vel bonorum scriptorem ad. Ei his nisl postea. Mei soleat periculis mnesarchum ex, ea sit theophrastus definitiones.</p>
+        </div>
+      </div>
+      </div>
+    </section>
     <?php include 'includes/footer.php'; ?>
 
 
